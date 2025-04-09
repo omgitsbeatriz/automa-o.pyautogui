0402dec3f6e93040d94af22b6630d6d342046a28
Beatriz Rodrigues <88855134+omgitsbeatriz@users.noreply.github.com>
2025-04-09 17:02:29 -0300

    Add files via upload

---
+++ Projeto de Automação.py
@@ -0,0 +1,46 @@
+import pyautogui
+import time
+import pandas as pd
+
+# Configura uma pausa padrão entre as ações do PyAutoGUI
+pyautogui.PAUSE = 2
+
+# Abre o navegador e acessa o site
+pyautogui.press('win')                # Abre o menu iniciar
+pyautogui.write('chrome')             # Digita "chrome"
+pyautogui.press('enter')              # Abre o navegador
+pyautogui.hotkey('ctrl', 'n')         # Abre uma nova janela
+pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
+pyautogui.press('enter')              # Acessa o link
+
+# Aguarda o site carregar
+time.sleep(5)
+
+# Login no site
+pyautogui.click(x=856, y=400)         # Clica no campo de e-mail
+pyautogui.write('email.ficticio@gmail.com')  # Digita o e-mail
+pyautogui.press('tab')                # Vai para o campo de senha
+pyautogui.write('senhaficticia')      # Digita a senha
+pyautogui.press('tab')                # Vai para o botão de login
+pyautogui.press('enter')              # Realiza o login
+
+# Leitura da planilha de produtos
+tabela = pd.read_csv(r'/home/beatriz/Documentos/Intensivo Python/produtos.csv')
+
+# Preenche o formulário para cada produto
+for linha in tabela.index:
+    pyautogui.click(x=868, y=288)  # Clica no primeiro campo do formulário
+    
+    campos = [
+            'codigo', 'marca', 'tipo', 'categoria',
+            'preco_unitario', 'custo', 'obs'
+        ]
+
+    for campo in campos:
+            valor = str(tabela.loc[linha, campo])
+            pyautogui.write(valor)
+            pyautogui.press('tab')
+
+    pyautogui.press('enter')  # Envia o formulário
+    time.sleep(1)
+    pyautogui.press('home')   # Volta ao topo
---
+++ auxiliar.py
@@ -0,0 +1,5 @@
+import pyautogui
+import time
+
+time.sleep(15)
+print(pyautogui.position())
